fd7d83a61acbafbfe014af092c38554a6c7c7b9c
Your Name <you@example.com>
2017-06-10 23:53:22 +0200

    expose virt_to_mfn() for xen guests

--- xen/lib/mM.ml
+++ xen/lib/mM.ml
@@ -2,3 +2,5 @@ module Heap_pages = struct
   external total: unit -> int = "stub_heap_get_pages_total" "noalloc"
   external used: unit -> int = "stub_heap_get_pages_used" "noalloc"
 end
+
+external virt_to_mfn: nativeint -> nativeint = "stub_virt_to_mfn"
